bd169f81d22d46e72fb1eb9d59ac00a9f68395f7
Nicolas Heguaburu <nicoheguaburu@gmail.com>
2022-08-31 22:14:11 -0300

    Segunda entrega

--- src/components/ListItems/ListItems.js
+++ src/components/ListItems/ListItems.js
@@ -1,9 +1,31 @@
 import './ListItems.css';
 
+import React,{useState} from 'react';
+
 const ListItems = () => {
+    const [cantidad, setCantidad] = useState(1);
+
+    const stock = 10;
+
+    const reducir = () => {
+        setCantidad(cantidad - 1);
+    }
+
+    const aumentar = () => {
+        setCantidad(cantidad + 1);
+    }
+
     return (
-        <div class= "listitems-container">
-            <p class="items">Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi delectus porro quam non dolore maiores earum laboriosam, ab est deleniti vero cum a. Nemo vitae fugiat velit quasi sapiente eum.</p>
+        <div className="contador">
+            <h2>{stock} Disponible</h2>
+            <div>
+            <button disabled={cantidad <= 1} onClick={reducir}>-</button>
+            <span>{cantidad}</span>
+            <button disabled={cantidad >= stock} onClick={aumentar}>+</button>
+            </div>
+            <div>
+                <button>Agregar al carrito</button>
+            </div>
         </div>
     );
 };
