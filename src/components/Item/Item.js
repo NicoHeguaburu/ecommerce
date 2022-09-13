bdf0e1c24e65b9a25a61aa6f2b5f82f146dc2145
Nicolas Heguaburu <nicoheguaburu@gmail.com>
2022-09-13 11:29:27 -0300

    falta poco

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -5,15 +5,16 @@ import './Item.css';
 const Item = ({title, price, description, imgUrl, id}) => {
 
     return (
-            <div>
-                <img href={imgUrl}/>
-                <h2>{title}dada</h2>
-                <p>{price}add</p>
-                <p>{description}adad</p>
+            <div className="item">
+                <img src={imgUrl}/>
+                <h2>{title}</h2>
+                <p>{price}</p>
+                <p>{description}</p>
+                <button></button>
             </div>
-    )
+    );
 
-    }
+};
 
 
 
